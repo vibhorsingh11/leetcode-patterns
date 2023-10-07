97a990737d34f48f36092de5898135181c519b9c
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-10-07 21:05:14 +0530

    Added Problem 107 Unique Paths

---
+++ JavaScript/leetcode150/src/main/Problem107_UniquePaths.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  const grid = new Array(m).fill(0).map(() => new Array(n));
+
+  // only one way to reach any cell in the first row or first column
+  for (let i = 0; i < m; i++) {
+    grid[i][0] = 1;
+  }
+
+  for (let j = 0; j < n; j++) {
+    grid[0][j] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+    }
+  }
+
+  return grid[m - 1][n - 1];
+};
+
+module.exports = uniquePaths;
---
+++ JavaScript/leetcode150/src/test/Problem107_UniquePaths.test.js
@@ -0,0 +1,5 @@
+const uniquePaths = require('../main/Problem107_UniquePaths');
+
+test('should return counts', () => {
+  expect(uniquePaths(3, 7)).toBe(28);
+});
