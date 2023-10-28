eafff5cd6c12e396a41570fee2c38948ee6702b0
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-10-28 22:07:07 +0530

    updated Valid Palindrome

--- JavaScript/leetcode150/src/test/02-TwoPointers/Problem10_ValidPalindrome.test.js
+++ JavaScript/leetcode150/src/test/02-TwoPointers/Problem10_ValidPalindrome.test.js
@@ -1,4 +1,4 @@
-const isPalindrome = require('../main/Problem10_ValidPalindrome');
+const isPalindrome = require('../../main/02-TwoPointers/Problem10_ValidPalindrome');
 
 test('should return if valid palindrome', () => {
   const s = 'A man, a plan, a canal: Panama';
