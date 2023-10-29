58cc34f7744bd2827278aa54060b455d550b0fc6
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-10-29 21:27:39 +0530

    updated 3 sum

--- JavaScript/leetcode150/src/test/02-TwoPointers/Problem12_3Sum.test.js
+++ JavaScript/leetcode150/src/test/02-TwoPointers/Problem12_3Sum.test.js
@@ -1,4 +1,4 @@
-const threeSum = require('../main/Problem12_3Sum');
+const threeSum = require('../../main/02-TwoPointers/Problem12_3Sum');
 
 test('should return triplets', () => {
   const nums = [-1, 0, 1, 2, -1, -4];
