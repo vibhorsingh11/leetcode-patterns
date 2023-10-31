12294affd47119c336dbc051f1103ef2449a3ec9
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-10-31 21:51:15 +0530

    updated imports

--- JavaScript/leetcode150/src/test/02-TwoPointers/Problem13_ContainerWithMostWater.test.js
+++ JavaScript/leetcode150/src/test/02-TwoPointers/Problem13_ContainerWithMostWater.test.js
@@ -1,4 +1,4 @@
-const maxArea = require('../main/Problem13_ContainerWithMostWater');
+const maxArea = require('../../main/02-TwoPointers/Problem13_ContainerWithMostWater');
 
 test('should return max area for water', () => {
   const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
--- JavaScript/leetcode150/src/test/02-TwoPointers/Problem14_TrappingRainWater.test.js
+++ JavaScript/leetcode150/src/test/02-TwoPointers/Problem14_TrappingRainWater.test.js
@@ -1,4 +1,4 @@
-const trap = require('../main/Problem14_TrappingRainWater');
+const trap = require('../../main/02-TwoPointers/Problem14_TrappingRainWater');
 
 test('should return max trap water', () => {
   const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
