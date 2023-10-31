deb2cf058c79da8cc3914527181bd40c6ec35033
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-10-31 21:49:41 +0530

    updated Best Time to Buy And Sell Stock

--- JavaScript/leetcode150/src/test/03-SlidingWindow/Problem01_BestTimeToBuyAndSellStock.test.js
+++ JavaScript/leetcode150/src/test/03-SlidingWindow/Problem01_BestTimeToBuyAndSellStock.test.js
@@ -1,4 +1,4 @@
-const maxProfit = require('../main/Problem15_BestTimeToBuyAndSellStock');
+const maxProfit = require('../../main/03-SlidingWindow/Problem01_BestTimeToBuyAndSellStock');
 
 test('should return max profit', () => {
   const prices = [7, 1, 5, 3, 6, 4];
