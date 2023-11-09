4332c17c1c3a8a25798f46d97de2937456bbd505
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-11-09 19:41:05 +0530

    updated Largest Rectangle In Histogram

--- JavaScript/leetcode150/src/test/04-Stack/Problem07_LargestRectangleInHistogram.test.js
+++ JavaScript/leetcode150/src/test/04-Stack/Problem07_LargestRectangleInHistogram.test.js
@@ -1,4 +1,4 @@
-const largestRectangleArea = require('../main/Problem27_LargestRectangleInHistogram');
+const largestRectangleArea = require('../../main/04-Stack/Problem07_LargestRectangleInHistogram');
 
 test('should return largest Rectangle Area', () => {
   const heights = [2, 1, 5, 6, 2, 3];
