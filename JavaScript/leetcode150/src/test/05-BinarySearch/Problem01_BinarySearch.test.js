8e526d264670052c45c189e6e3d803889f1389fa
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-11-10 21:35:06 +0530

    updated Binary Search

--- JavaScript/leetcode150/src/test/05-BinarySearch/Problem01_BinarySearch.test.js
+++ JavaScript/leetcode150/src/test/05-BinarySearch/Problem01_BinarySearch.test.js
@@ -1,4 +1,4 @@
-const search = require('../main/Problem28_BinarySearch');
+const search = require('../../main/05-BinarySearch/Problem01_BinarySearch');
 
 test('should return index', () => {
   const nums = [-1, 0, 3, 5, 9, 12],
