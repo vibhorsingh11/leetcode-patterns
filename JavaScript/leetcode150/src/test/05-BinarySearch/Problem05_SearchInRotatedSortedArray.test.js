96613f256b15d547f8d60aca5c7a56479261bc38
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-11-14 20:57:24 +0530

    updated Search In Rotated Sorted Array

--- JavaScript/leetcode150/src/test/05-BinarySearch/Problem05_SearchInRotatedSortedArray.test.js
+++ JavaScript/leetcode150/src/test/05-BinarySearch/Problem05_SearchInRotatedSortedArray.test.js
@@ -1,4 +1,4 @@
-const search = require('../main/Problem32_SearchInRotatedSortedArray');
+const search = require('../../main/05-BinarySearch/Problem05_SearchInRotatedSortedArray');
 
 test('should return index of target', () => {
   const nums = [4, 5, 6, 7, 0, 1, 2],
