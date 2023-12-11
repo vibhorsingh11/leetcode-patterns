9cba9215426b8152044e066dedff870eda0a30f5
vibhorsingh11 <vibhorsinghcs@gmail.com>
2023-12-11 22:31:43 +0530

    Updated Target sum

--- JavaScript/leetcode150/src/test/14-DP-2D/Problem03_TargetSum.test.js
+++ JavaScript/leetcode150/src/test/14-DP-2D/Problem03_TargetSum.test.js
@@ -1,4 +1,4 @@
-const findTargetSumWays = require('../../main/14-DP-2D/Problem111_TargetSum');
+const findTargetSumWays = require('../../main/14-DP-2D/Problem03_TargetSum');
 
 test('should return number of ways', () => {
   const nums = [1, 1, 1, 1, 1],
